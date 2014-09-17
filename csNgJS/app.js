6255626538d259408bf60103da5b23a61fac04d2
Anthony Chao <anthonyqchao@gmail.com>
2014-09-17 00:23:36 -0700

    i don't even know

--- csNgJS/app.js
+++ csNgJS/app.js
@@ -5,60 +5,6 @@
 		this.products = gems;
 	});
 
-	var gems = [{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Deodecahedron is one of those gems.',
-			canPurchase: true,
-			soldOut: true,
-			images: [
-				'images/gem-01.gif'
-			],
-			reviews: [{
-				stars: 4,
-				body: 'This gem is truely amazing',
-				author: 'Lil B'
-			},
-			{
-				stars: 4,
-				body: 'This gem is truely amazing',
-				author: 'Lil B'
-			},
-			{
-				stars: 4,
-				body: 'This gem is truely amazing',
-				author: 'Lil B'
-			}
-			]
-		},
-		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: 'Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.',
-			canPurchase: false,
-			soldOut: false,
-			images: [
-				'images/gem-02.gif'
-			],
-			reviews: [{
-				stars: 4,
-				body: 'This gem is truely amazing',
-				author: 'Lil B'
-			},
-			{
-				stars: 4,
-				body: 'This gem is truely amazing',
-				author: 'Lil B'
-			},
-			{
-				stars: 4,
-				body: 'This gem is truely amazing',
-				author: 'Lil B'
-			}
-			]
-		}
-	];
-
 	app.controller("PanelController", function() {
 		this.tab = 1;
 
@@ -75,9 +21,87 @@
 		this.review = {};
 
 		this.addReview = function(product){
+			this.review.createdOn = Date.now();
 			product.reviews.push(this.review);
 			this.review = {};
 		};
 	});
+
+	// Custom Directives - wack-ass not working
+	/*
+	app.directive('productReviews', function() {
+		return {
+			restrict: "E",
+			templateUrl: "product-reviews.html"
+		};
+	});
+
+	app.directive('productSpecs', function() {
+		return {
+			restrict: "A",
+			templateUrl: "product-specs.html"
+		};
+	});
+	*/
+
+
+	var gems = [{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Deodecahedron is one of those gems.',
+		canPurchase: true,
+		soldOut: false,
+		shine: 8,
+		rarity: 7,
+		faces: 12,
+		images: [
+			'images/gem-01.gif'
+		],
+		reviews: [{
+			stars: 4,
+			body: 'This gem is truely amazing',
+			author: 'Lil B'
+		},
+		{
+			stars: 4,
+			body: 'This gem is truely amazing',
+			author: 'Lil B'
+		},
+		{
+			stars: 4,
+			body: 'This gem is truely amazing',
+			author: 'Lil B'
+		}
+		]
+	},
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: 'Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.',
+		canPurchase: true,
+		soldOut: false,
+		shine: 10,
+		rarity: 10,
+		faces: 5,
+		images: [
+			'images/gem-02.gif'
+		],
+		reviews: [{
+			stars: 4,
+			body: 'This gem is truely amazing',
+			author: 'Lil B'
+		},
+		{
+			stars: 4,
+			body: 'This gem is truely amazing',
+			author: 'Lil B'
+		},
+		{
+			stars: 4,
+			body: 'This gem is truely amazing',
+			author: 'Lil B'
+		}
+		]
+	}];
 
 })();
